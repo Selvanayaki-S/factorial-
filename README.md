c1b3516a09505d0ba5ec316de49ad714e36ca9d5
Selvanayaki <s.selvanayaki.be@gmail.com>
2023-12-19 11:56:30 +0530

    code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# factorial-
+# FACORIAL OF A GIVEN NUMBER
+
+## AIM
+
+TO FIND FACTORIAL
+
+## PROCEDURE
+
+## CODE
+
+## RESULT
---
+++ fact.py
@@ -0,0 +1,9 @@
+import os
+def factorial(n):
+    if(n==0):
+        return 1
+    return n*factorial(n-1)
+
+n=input()
+print(factorial(n))
+
